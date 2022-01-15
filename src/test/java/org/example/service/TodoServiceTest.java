f31d273222888df437f52d817eff49b3c2b6984f
eros21c <eros21c@outlook.com>
2022-01-16 02:03:31 +0900

    Initial commit

---
+++ src/test/java/org/example/service/TodoServiceTest.java
@@ -0,0 +1,15 @@
+package org.example.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith
+class TodoServiceTest {
+
+    @org.junit.jupiter.api.Test
+    void add() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void searchById() {
+    }
+}
