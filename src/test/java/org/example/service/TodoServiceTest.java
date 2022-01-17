35f84103fe3a98c9dc0389fe2ece2cade3501fa9
eros21c <eros21c@outlook.com>
2022-01-18 01:54:35 +0900

    TodoServer Test 코드 추가

--- src/test/java/org/example/service/TodoServiceTest.java
+++ src/test/java/org/example/service/TodoServiceTest.java
@@ -1,15 +1,76 @@
 package org.example.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.example.model.TodoEntity;
+import org.example.model.TodoRequest;
+import org.example.repository.TodoRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.AdditionalAnswers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.server.ResponseStatusException;
 
-@ExtendWith
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class TodoServiceTest {
 
-    @org.junit.jupiter.api.Test
+    @Mock
+    private TodoRepository todoRepository;
+
+    @InjectMocks
+    private TodoService todoService;
+
+    @Test
     void add() {
+        when(this.todoRepository.save(any(TodoEntity.class)))
+                .then(AdditionalAnswers.returnsFirstArg());
+
+        TodoRequest expected = new TodoRequest();
+        expected.setTitle("Test Title");
+
+        TodoEntity actual = this.todoService.add(expected);
+
+        assertEquals(expected.getTitle(), actual.getTitle());
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void searchById() {
+        TodoEntity todoEntity = new TodoEntity();
+        todoEntity.setId(123L);
+        todoEntity.setTitle("TITLE");
+        todoEntity.setOrder(0L);
+        todoEntity.setCompleted(false);
+
+        Optional<TodoEntity> optional = Optional.of(todoEntity);
+
+        given(this.todoRepository.findById(anyLong()))
+                .willReturn(optional);
+
+        TodoEntity actual= this.todoService.searchById(todoEntity.getId());
+
+        TodoEntity expected = optional.get();
+
+        assertEquals(actual.getId(), expected.getId());
+        assertEquals(actual.getTitle(), expected.getTitle());
+        assertEquals(actual.getOrder(), expected.getOrder());
+        assertEquals(actual.getCompleted(), expected.getCompleted());
+    }
+
+    @Test
+    public void searchBydFailed() {
+        given(this.todoRepository.findById(anyLong()))
+                .willReturn(Optional.empty());
+
+        assertThrows(ResponseStatusException.class, () -> {
+            this.todoService.searchById(123L);
+        });
     }
 }
